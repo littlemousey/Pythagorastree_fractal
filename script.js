87512ce739c16d347eda10d2155606e1093b08ea
Ans de Nijs <ansdenijs@gmail.com>
2017-02-03 21:46:32 +0100

    Pythagoras tree fix
    fixed, and colored

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 shrink = 0.5*Math.sqrt(2);
 
-var c = document.getElementById("myCanvas");
-var drawing = c.getContext("2d");
+var c = document.getElementById('myCanvas');
+var drawing = c.getContext('2d');
+
+var r = 0, g = 0, b = 0;
 
 var length = 100;
-var stop = 10;
-drawing.fillStyle = "#ff9900";
+var stop = 5;
+drawing.fillStyle = rgb(r, g, b);
+var count = 0;
 
 //origin van canvas op 300,300 zetten
-drawing.translate(300,300);
+drawing.translate(500,500);
 drawing.rotate(-Math.PI/4);
 
 
 function recursion(length){
+	drawing.fillStyle = rgb (count * 40, g, b);
 	// Teken de linkse
-	drawing.restore();
+	//drawing.restore();
+ 	drawing.save();
  	drawing.translate(length,0);
  	drawing.rotate(-Math.PI/4);
  	//teken linkerkant (bestond al)
  	drawing.fillRect(0,0,length*shrink,length*shrink);
- 	drawing.save();
 
  	// Ga door links
   	if(length > stop){
 		//nieuw
-		recursionRight(shrink*length);
+		//recursionRight(shrink*length);
 
+		count ++;
 		recursion(shrink * length);
+		count --;
 
 	};
-/*
-	// Teken de rechtse
+
 	drawing.restore();
+
+	// Teken de rechtse
+	drawing.translate(length,length);
 	drawing.rotate(Math.PI/4);
-	drawing.translate(0,length);
-	drawing.rotate(-Math.PI/4);
+	drawing.translate(0,-length*shrink);
  	drawing.fillRect(0,0,length*shrink,length*shrink);
- 	drawing.save();
 
  	// Ga door rechts
   	if(length > stop){
+		count ++;
 		recursion(shrink * length);
-	};  
-	*/	
+		count --;
+	};
 };
 
 function recursionRight(length){
@@ -59,4 +66,8 @@ drawing.fillRect(0,0,length*shrink,length*shrink);
 
 
 // Teken de rest
-recursion(length *shrink);
+recursion(length * shrink);
+
+function rgb(r, g, b) {
+	return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
