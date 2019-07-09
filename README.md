eb00b6eddfcd4b9e85ea9a29b431e6789f68ff01
Ans de Nijs <ansdenijs@gmail.com>
2019-07-09 12:07:39 +0200

    Updated styling and readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-Trying to create a Pythagoras tree fractal in Javascript
-For more information view https://en.wikipedia.org/wiki/Pythagoras_tree_%28fractal%29
+#My own Pythagoras tree fractal in Javascript
+This was an old project of mine, when I was starting out writing JavaScript. Thank you for your help [JoostK](https://github.com/JoostK) and [bdupau](https://github.com/bdupau)
+For more information on Pythagoras trees, view [Wikipedia](https://en.wikipedia.org/wiki/Pythagoras_tree_%28fractal%29)
+
+![Pythagoras tree](fractal.png)
 
 -Little mousey
--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-shrink = 0.5*Math.sqrt(2);
+shrink = 0.5 * Math.sqrt(2);
 
-var c = document.getElementById('myCanvas');
-var drawing = c.getContext('2d');
+var c = document.getElementById("myCanvas");
+var drawing = c.getContext("2d");
 
-var r = 0, g = 0, b = 0;
+var r = 0,
+  g = 0,
+  b = 0;
 
 var length = 100;
-var stop = 5;
+var stop = 1;
 drawing.fillStyle = rgb(r, g, b);
 var depth = 0;
 
 // Origin van canvas
-drawing.translate(600,600);
-drawing.rotate(-Math.PI/2);
+drawing.translate(400, 400);
+drawing.rotate(-Math.PI / 2);
 
 // Teken de eerste
-drawing.translate(length,0);
-drawing.fillRect(0,0,length*shrink,length*shrink);
+drawing.translate(length, 0);
+drawing.fillRect(0, 0, length * shrink, length * shrink);
 
+function recursion(length, depth) {
+  drawing.fillStyle = rgb(depth * 5, depth * 25, depth * 5);
+  // Teken de linker
+  drawing.save();
+  drawing.translate(length, 0);
+  drawing.rotate(-Math.PI / 4);
+  drawing.fillRect(0, 0, length * shrink, length * shrink);
 
-function recursion(length, depth){
-	drawing.fillStyle = rgb (depth * 40, g, b);
-	// Teken de linker
- 	drawing.save();
- 	drawing.translate(length,0);
- 	drawing.rotate(-Math.PI/4);
- 	drawing.fillRect(0,0,length*shrink,length*shrink);
+  // Ga door links
+  if (length > stop) {
+    recursion(shrink * length, depth + 1);
+  }
 
- 	// Ga door links
-  	if(length > stop){
-		recursion(shrink * length, depth + 1);
+  drawing.restore();
 
-	};
+  // Teken de rechter
+  drawing.translate(length, length);
+  drawing.rotate(Math.PI / 4);
+  drawing.translate(0, -length * shrink);
+  drawing.fillRect(0, 0, length * shrink, length * shrink);
 
-	drawing.restore();
-
-	// Teken de rechter
-	drawing.translate(length,length);
-	drawing.rotate(Math.PI/4);
-	drawing.translate(0,-length*shrink);
- 	drawing.fillRect(0,0,length*shrink,length*shrink);
-
- 	// Ga door rechts
-  	if(length > stop){
-		recursion(shrink * length, depth + 1);
-	};
-};
+  // Ga door rechts
+  if (length > stop) {
+    recursion(shrink * length, depth + 1);
+  }
+}
 
 // Teken de rest
 recursion(length * shrink, depth);
 
 function rgb(r, g, b) {
-	return 'rgb(' + r + ',' + g + ',' + b + ')';
+  return "rgb(" + r + "," + g + "," + b + ")";
 }
